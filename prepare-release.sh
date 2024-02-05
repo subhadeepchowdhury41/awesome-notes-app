dc9ee18b75f5fc3b513d7ab1c5bdeeeb86aa07dc
subhadeepchowdhury41 <subhadeepchowdhury41@gmail.com>
2024-02-05 10:22:16 +0530

    release

---
+++ prepare-release.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Check if directory parameter is provided
+if [ $# -eq 0 ]; then
+  echo "Usage: $0 <directory_name>"
+  exit 1
+fi
+
+# flutter build apk --release
+
+# Assign directory name from command line argument
+dir="$1"
+
+# Check if release directory exists, if not create it
+if [ ! -d "release" ]; then
+  mkdir release
+fi
+
+# Copy the APK file to the specified directory
+cp build/app/outputs/flutter-apk/app-release.apk release/"${dir}"
+
+echo "APK file copied to release/${dir}"
